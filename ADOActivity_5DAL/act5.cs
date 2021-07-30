38d2439a9827df24a920b70750f4f2eb447ad947
Pranali21699 <pranali.khilari@ltts.com>
2021-07-30 18:39:45 +0530

    Add project files.

---
+++ ADOActivity_5DAL/act5.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOActivity_5DAL
+{
+    public class act5
+    {
+        SqlConnection sqlConObj;
+        SqlCommand sqlCmdObj;
+
+        public string ConnectToDB()
+        {
+            try
+            {
+                sqlConObj = new SqlConnection(ConfigurationManager.ConnectionStrings["abc"].ToString());
+                //string conStr = ConfigurationManager.ConnectionStrings["AdvConStr"].ToString();
+                sqlConObj.Open();
+                return sqlConObj.State.ToString();
+            }
+            catch (Exception)
+            {
+                return sqlConObj.State.ToString();
+
+            }
+            finally
+            {
+                sqlConObj.Close();
+            }
+
+        }
+        public string GetFullName(int inputID)
+        {
+            string result = String.Empty;
+           sqlConObj = new SqlConnection(ConfigurationManager.ConnectionStrings["abc"].ToString());
+            //sqlCmdObj = new SqlCommand(@"SELECT [dbo].[ufnGetFullName]("+id+")", sqlConObj);
+            sqlCmdObj = new SqlCommand(@"SELECT [dbo].[ufnGetFullName](@ID)", sqlConObj);
+            sqlCmdObj.Parameters.AddWithValue("@ID", inputID);
+            //sqlCmdObj.Parameters.Add()
+            try
+            {
+                sqlConObj.Open();
+                result = Convert.ToString(sqlCmdObj.ExecuteScalar());
+                return result;
+            }
+            catch (Exception ex)
+            {
+                result = "Error occurred";
+                return result;
+                //throw;
+            }
+        }
+    }
+}
---
+++ ADOActivity_5UI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADOActivity_5DAL;
+
+namespace ADOActivity_5UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            act5 obj = new act5();
+            Console.WriteLine(obj.GetFullName(1));
+        }
+    }
+}
